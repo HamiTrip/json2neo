8a152a5232df56312105d2dec54c70b7698194e2
Saeed Falsafin <sabloger@gmail.com>
2017-09-10 13:00:13 +0430

    Beta version is completed, insert and retrieve!

--- consts.go
+++ consts.go
@@ -1,20 +1,23 @@
 package json2neo
 
 const (
-	L_HAS_NESTED       = "JN_HAS_NESTED"
-	L_ROOT_NODE        = "JN_ROOT_NODE"
-	L_OBJ_PROP         = "JN_OBJ_PROP"
-	L_ARR_PROP         = "JN_ARR_PROP"
-	TYPE_ARRAY         = "array"
-	TYPE_OBJECT        = "object"
-	VAR_STUB           = "stub"
-	VAR_ROOT           = "root"
-	ROOT_NAME_KEY      = "jn_name"
+	L_HAS_NESTED  = "JN_HAS_NESTED"
+	L_ROOT_NODE   = "JN_ROOT_NODE"
+	L_OBJ_PROP    = "JN_OBJ_PROP"
+	L_ARR_PROP    = "JN_ARR_PROP"
+	TYPE_ARRAY    = "array"
+	TYPE_OBJECT   = "object"
+	VAR_STUB      = "stub"
+	VAR_ROOT      = "root"
+	ROOT_NAME_KEY = "jn_name"
+	TYPE_KEY      = "jn_type"
+	DATA_KEY      = "jn_data"
+	LABELS_KEY    = "jn_labels"
 )
 
 var TypeToLabel = map[string]string{
-	TYPE_ARRAY:L_ARR_PROP,
-	TYPE_OBJECT:L_OBJ_PROP,
-	L_ARR_PROP:TYPE_ARRAY,
-	L_OBJ_PROP:TYPE_OBJECT,
+	TYPE_ARRAY:  L_ARR_PROP,
+	TYPE_OBJECT: L_OBJ_PROP,
+	L_ARR_PROP:  TYPE_ARRAY,
+	L_OBJ_PROP:  TYPE_OBJECT,
 }
