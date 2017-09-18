07ddade26851f20f116eea7f9e4267a62c10fdd2
Saeed Falsafin <sabloger@gmail.com>
2017-09-18 17:07:39 +0430

    :art: Code clearing!

--- consts.go
+++ consts.go
@@ -2,30 +2,30 @@ package json2neo
 
 /*
 Constants of general cypher parts
- */
+*/
 const (
 	LabelHasNested = "JN_HAS_NESTED"
-	LabelRootNode = "JN_ROOT_NODE"
-	LabelObjProp = "JN_OBJ_PROP"
-	LabelArrProp = "JN_ARR_PROP"
-	TypeArray = "array"
-	TypeObject = "object"
-	VarStub = "stub"
-	VarRoot = "root"
-	IDKey = "_id"
-	RootNameKey = "jn_name"
-	TypeKey = "jn_type"
-	DataKey = "jn_data"
-	LabelsKey = "jn_labels"
-	ValueTrue = "true"
+	LabelRootNode  = "JN_ROOT_NODE"
+	LabelObjProp   = "JN_OBJ_PROP"
+	LabelArrProp   = "JN_ARR_PROP"
+	TypeArray      = "array"
+	TypeObject     = "object"
+	VarStub        = "stub"
+	VarRoot        = "root"
+	IDKey          = "_id"
+	RootNameKey    = "jn_name"
+	TypeKey        = "jn_type"
+	DataKey        = "jn_data"
+	LabelsKey      = "jn_labels"
+	ValueTrue      = "true"
 )
 
 /*
 TypeToLabel is map of labels to type and vise versa
- */
+*/
 var TypeToLabel = map[string]string{
-	TypeArray:  LabelArrProp,
-	TypeObject: LabelObjProp,
-	LabelArrProp:  TypeArray,
-	LabelObjProp:  TypeObject,
+	TypeArray:    LabelArrProp,
+	TypeObject:   LabelObjProp,
+	LabelArrProp: TypeArray,
+	LabelObjProp: TypeObject,
 }
